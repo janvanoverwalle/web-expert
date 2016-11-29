659a9f3fcd8b2f09adc98c7d09a8916dc6e3bb7b
Jan Van Overwalle <janvanoverwalle@gmail.com>
2016-11-29 20:34:21 +0100

    Added File Linker
    Added an automatic exercise url linker: scans the directories for
    exercises and displays them as <a>tags

---
+++ exercises/dir.php
@@ -0,0 +1,56 @@
+<?php
+	if (file_exists(dirname(__DIR__).'\\index.html')) {
+		echo "<div>", "\r\n", ahref('../index.html', 'Index'), "\r\n", "</div>", "\r\n";
+	}
+	
+	if (file_exists(__DIR__.'\\vb')) {
+		echo "<div>", "\r\n";
+		echo h('Voorbeelden', 2), "\r\n";
+		if (file_exists(__DIR__.'\\vb\\index.html')) {
+			$list = array(ahref('vb/index.html', 'Voorbeeld'));
+			echo ulist($list);
+		}
+		echo "</div>", "\r\n";
+	}
+	
+	for ($currentChapter  = 1; $currentChapter  < 9; $currentChapter ++) {
+		$currentChapterString = sprintf('H%02d', $currentChapter);
+		$exercises = array();
+		
+		if (file_exists(__DIR__.'\\'.$currentChapterString.'')) {
+			echo "<div>", "\r\n";
+			echo h('Hoofdstuk '.$currentChapter, 2), "\r\n";
+			$filename = strtolower($currentChapterString).'oef'.sprintf('%02d', count($exercises) + 1).'.html';
+			
+			while (file_exists(__DIR__.'\\'.$currentChapterString.'\\'.$filename)) {
+				$text = 'Oefening '.(count($exercises) + 1);
+				$exercises[] = ahref($currentChapterString.'/'.$filename, $text);
+				$filename = strtolower($currentChapterString).'oef'.sprintf('%02d', count($exercises) + 1).'.html';
+			}
+			echo ulist($exercises);
+			echo "</div>", "\r\n";
+		}
+	}
+	
+	function h($content, $lvl) {
+		return "<h".$lvl.">".$content."</h".$lvl.">";
+	}
+	
+	function ahref($url, $text) {
+		return "<a href='".$url."'>".$text."</a>";
+	}
+	
+	function ulist($items) {
+		if (empty($items)) {
+			return "";
+		}
+		
+		$result = "<ul>\r\n";
+		foreach ($items as $item) {
+			$result .= "<li>".$item."</li>\r\n";
+		}
+		$result .= "</ul>\r\n";
+		
+		return $result;
+	}
+?>
---
+++ exercises/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Oefeningen | Index</title>
+		<link href="css/bootstrap.css" rel="stylesheet" />
+	</head>
+	<body class="container">
+		<?php include 'dir.php'; ?>
+	</body>
+</html>
