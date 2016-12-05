7e154d86608d02d407c059b3b74ae451717adb99
Jan Van Overwalle <janvanoverwalle@gmail.com>
2016-12-05 17:28:10 +0100

    Added exercises

--- exercises/H03/h03.js
+++ exercises/H03/h03.js
@@ -20,12 +20,27 @@ app.controller('oef01',
 
 app.controller('oef02',
 	function ($scope) {
-		$scope.sum = function (article) {
-			alert(article.description + "\r\nVerkoopprijs: " + article.sellprice + "\r\nAankoopprijs: " + article.buyprice);
+		$scope.sum = function () {
+			$scope.result = parseInt($scope.a) + parseInt($scope.b);
 		}
 	}
 );
 
-//TODO: finish
-// Check here for help
-// https://docs.angularjs.org/guide/forms
+app.controller('oef03', [oef03Controller]);
+function oef03Controller() {
+	var _this = this;
+	
+	// 1. array met artikelen
+	_this.articles = [
+		{ number: '000001', description: 'Tandenborstel', sellprice: 3.25, buyprice: 1.50 },
+		{ number: '000002', description: 'Handdoek', sellprice: 4.75, buyprice: 2.50 },
+		{ number: '000003', description: 'Washandje', sellprice: 1.20, buyprice: 0.40 },
+		{ number: '000004', description: 'Keukenrol', sellprice: 1.75, buyprice: 0.65 },
+		{ number: '000005', description: 'Tandpasta', sellprice: 0.65, buyprice: 0.15 },
+	];
+
+	// 2. klikken op een artikel afvangen
+	_this.onClickArticle = function (article) {
+		alert(article.description + "\r\nVerkoopprijs: " + article.sellprice + "\r\nAankoopprijs: " + article.buyprice);
+	};
+};
